9a2287b7468138ada1d9a2f62f43df7763fbe8e8
Bruno Jouhier <bjouhier@gmail.com>
2014-05-24 20:49:17 +0200

    first brew

---
+++ README.md
@@ -0,0 +1,40 @@
+# Fast incremental JSON parser
+
+i-json is a fast incremental JSON parser written implemented in pure JS. 
+
+See https://github.com/joyent/node/issues/7543#issuecomment-43974821 for background discussion
+
+## API
+
+```javascript
+var ijson = require('i-json');
+
+var parser = ijson.creatParser();
+
+// update the parser with the next piece of JSON text.
+// This call will typically be issued from a 'data' event handler
+parser.update(jsonChunk);
+
+// retrieve the result
+var obj = parser.result();
+```
+
+## Installation
+
+``` sh
+npm install i-json
+```
+
+# Performance
+
+Typical results of the test program on my MBP i7
+
+JSON: 622 ms
+I-JSON single chunk: 1661 ms
+I-JSON multiple chunks: 1800 ms
+
+It is a bit faster than json2, which is implemented with regexp sanitization and `eval`.
+
+# License
+
+[MIT license](http://en.wikipedia.org/wiki/MIT_License).
---
+++ test/test.js
@@ -0,0 +1,57 @@
+"use strict";
+var fs = require('fs');
+var ijson = require('../index');
+
+var big = '[' + fs.readFileSync(__dirname + '/big.json') + ']';
+var ITER = 10;
+
+function test(msg, fn, data) {
+	var result;
+	var t0 = Date.now();
+	for (var i = 0; i < ITER; i++) result = fn(data);
+	console.log(msg + ': ' + (Date.now() - t0) + " ms");
+	return result;
+}
+
+function check(r1, r2) {
+	var s1 = JSON.stringify(r1, null, '\t');
+	var s2 = JSON.stringify(r2, null, '\t');
+	if (s1 === s2) {
+		console.log("SAME RESULTS!");
+	} else {
+		console.log("DIFFERENT RESULTS!");
+		var a1 = s1.split('\n');
+		var a2 = s2.split('\n');
+		for (var i = 0; i < a1.length; i++) {
+			if (a1[i] !== a2[i]) {
+				console.log((i + 1) + ": a1=" + a1[i]);
+				console.log((i + 1) + ": a2=" + a2[i]);
+				break;
+			}
+		}
+	}
+}
+
+var r1 = test("JSON", JSON.parse, big);
+
+var r2 = test("I-JSON single chunk", function(data) {
+	var parser = ijson.createParser();
+	parser.update(big);
+	return parser.result();
+}, big);
+
+var r3 = test("I-JSON multiple chunks", function(data) {
+	var parser = ijson.createParser();
+	var pos = 0;
+	var len = big.length;
+	while (pos < len) {
+		var l = Math.floor(Math.random() * 1024) + 1;
+		if (pos + l > len) l = len - pos;
+		parser.update(big.substring(pos, pos + l));
+		pos += l;
+	}
+	return parser.result();
+}, big);
+
+check(r1, r2);
+check(r1, r3);
